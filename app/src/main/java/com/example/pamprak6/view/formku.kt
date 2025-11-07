d059c6c498de188dc25f59563d7bb4218fc563c7
Irfan Fauzi <irfanfauziyk@gmail.com>
2025-11-07 14:46:06 +0700

    membuat fungsi form data diri

--- app/src/main/java/com/example/pamprak6/view/formku.kt
+++ app/src/main/java/com/example/pamprak6/view/formku.kt
@@ -1,1 +1,26 @@
 package com.example.pamprak6.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pamprak6.R
+
+@Composable
+fun FormDataDiri(
+    modifier: Modifier = Modifier,
+    onSubmitClick: (String, String, String, String) -> Unit
+) {}
